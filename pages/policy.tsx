2210917f3bb6b98946009dd0607ce82161cf7933
Lam Quang Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2021-11-12 15:52:31 +0700

    Dummy policy and tos

---
+++ pages/policy.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {NextPage} from "next";
+
+const Policy: NextPage = () => {
+    return <div>
+        <h2>Privacy Policy</h2>
+        <p>This site uses JSON Web Tokens</p>
+
+        <p>Data provided to this site is exclusively used to support signing in and is not
+            passed to any third party services, other than via SMTP or OAuth
+            for the purposes of authentication.</p>
+    </div>
+}
+
+export default Policy
